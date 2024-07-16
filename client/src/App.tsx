faa11afcdb67ed16d354b125e7cb19247d848ee3
chinmayweb3 <144161893+chinmayweb3@users.noreply.github.com>
2024-07-17 00:22:02 +0530

    removed shadcn

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" index element={<Home />} />
-          {/* <Route path="/auth/login" element={<Login />} />
-          <Route path="/auth/register" element={<Register />} /> */}
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" index element={<Home />} />
+        <Route path="/auth/login" element={<Login />} />
+        <Route path="/auth/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
