20cfaa22babb9b2760800ebc6639633a99552c23
chinmayweb3 <144161893+chinmayweb3@users.noreply.github.com>
2024-07-16 22:14:56 +0530

    Updated auth/index.ts to add Firebase auth functionality for login and register routes

--- server/src/auth/index.ts
+++ server/src/auth/index.ts
@@ -1,13 +1,22 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
+import { fireAuth } from "../config/firebase";
+import { getAuth } from "firebase-admin/auth";
 dotenv.config();
 
 const router = express.Router();
 
 //get all document
-router.post("/login", (req: Request, res: Response) => {});
+router.post("/login", async (req: Request, res: Response) => {});
 
 //get all document
-router.post("/register", (req: Request, res: Response) => {});
+router.post("/register", async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  const user = await fireAuth.createUser({ email, password });
+
+  const jwt = await fireAuth.createCustomToken(user.uid);
+
+  res.status(200).json({ jwt });
+});
 
 export default router;
