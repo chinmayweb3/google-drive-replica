d194e0cd1c736d8ea5a0e11dc004ae9053794550
chinmayweb3 <144161893+chinmayweb3@users.noreply.github.com>
2024-07-16 20:41:09 +0530

    Added CORS middleware to server, updated package.json with new dependencies

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,19 @@
 import express, { Express, Request, Response } from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 import folderRouter from "./folder";
 import documentRouter from "./document";
 import authRouter from "./auth";
 dotenv.config();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 const app: Express = express();
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: "https://localhost:3000",
+  })
+);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
