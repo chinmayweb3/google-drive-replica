d9ddfea16a41a5e69058242b17edf81e13bbaa16
chinmayweb3 <144161893+chinmayweb3@users.noreply.github.com>
2024-07-17 13:45:43 +0530

    Removed console logs and updated status code check in register method of Auth class

--- client/src/api/auth.ts
+++ client/src/api/auth.ts
@@ -20,16 +20,13 @@ class Auth extends globalClass implements IAuth {
   constructor() {
     super();
   }
-
   async register(body: InputField): Promise<ResAuth> {
     const response = await this.fetchData("POST", "auth/register", {
       body,
     });
-    console.log("auth/register response", response);
-
     const resp = await response.json();
-    console.log("auth/register await", resp);
-    if (resp.status == 204) {
+
+    if (resp.status == 201) {
       return { status: "success", msg: resp.data };
     } else {
       return { status: "error", err: resp.data };
