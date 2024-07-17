95f05ca1f9c79b23b61e350d48ee530955659d43
chinmayweb3 <144161893+chinmayweb3@users.noreply.github.com>
2024-07-17 15:30:30 +0530

    "Wrapped App component with ReduxProvider"

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,17 +3,20 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Navbar from "./components/navbar";
+import { ReduxProvider } from "./config/reduxProvider";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" index element={<Home />} />
-        <Route path="/auth/login" element={<Login />} />
-        <Route path="/auth/register" element={<Register />} />
-      </Routes>
-    </BrowserRouter>
+    <ReduxProvider>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" index element={<Home />} />
+          <Route path="/auth/login" element={<Login />} />
+          <Route path="/auth/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
+    </ReduxProvider>
   );
 }
 
